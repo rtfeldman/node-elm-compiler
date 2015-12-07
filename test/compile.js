acac91937111fe91f88fabc39ed4c868fe1b7360
Richard Feldman <richard.t.feldman@gmail.com>
2015-12-07 13:43:40 -0800

    Add a basic test for #compile

---
+++ test/compile.js
@@ -0,0 +1,32 @@
+var assert = require("chai").assert;
+var path = require("path");
+var compiler = require(path.join(__dirname, ".."));
+var childProcess = require("child_process");
+var _ = require("lodash");
+
+var fixturesDir = path.join(__dirname, "fixtures");
+
+function prependFixturesDir(filename) {
+  return path.join(fixturesDir, filename);
+}
+
+function spawnForFixtures(pathToMake, processArgs, processOpts) {
+  var opts = _.defaults({cwd: fixturesDir}, processOpts);
+
+  return childProcess.spawn(pathToMake, processArgs, opts);
+}
+
+describe("#compile", function() {
+  it("works with --yes", function (done) {
+    // Use a timeout of 30 seconds because we need to download packages.
+    this.timeout(30000);
+
+    var opts = {yes: true, output: "/dev/null", verbose: true, spawn: spawnForFixtures};
+    var compileProcess = compiler.compile(prependFixturesDir("Parent.elm"), opts);
+
+    compileProcess.on("close", function(exitCode) {
+      assert.equal(exitCode, 0, "Expected elm-make to have exit code 0");
+      done();
+    });
+  });
+});
