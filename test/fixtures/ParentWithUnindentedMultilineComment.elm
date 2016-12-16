6f9c2ee2aaed1e756b4142778a307b90f66f9f2f
eeue56 <enalicho@gmail.com>
2016-12-16 17:22:18 +0100

    fix tests

--- test/fixtures/ParentWithUnindentedMultilineComment.elm
+++ test/fixtures/ParentWithUnindentedMultilineComment.elm
@@ -1,13 +1,13 @@
 module ParentWithUnindentedMultilineComment exposing (..)
 
-import Test.ChildA
-
-
 {-| This is a comment in between two import statements.
 sometimes I write things here because I like to watch the world burn
 -}
+import Test.ChildA
 
-import Test.ChildB exposing (..) {-| this comment will end before an import
+
+import Test.ChildB exposing (..)
+{- this comment will end before an import
 -}
 import Native.Child exposing (..)
 import Html
