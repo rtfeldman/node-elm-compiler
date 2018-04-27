080c74f9eea0b1086567a05f43fce0fb23873688
Richard Feldman <richard.t.feldman@gmail.com>
2018-04-26 21:38:58 -0400

    Update test for new compiler error.

--- test/compile.js
+++ test/compile.js
@@ -73,7 +73,7 @@ describe("#compileToString", function() {
       expect(err).to.be.an('error');
       expect(String(err))
         .to.contain("Compilation failed")
-        .and.contain("SYNTAX PROBLEM");
+        .and.contain("PARSE ERROR");
     });
   });
 
