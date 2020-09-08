a561b643b2d0a520df4a30f27eb88a1bf3a07cb9
Johannes Maas <y0hy0h@gmx.net>
2020-09-08 18:25:03 +0200

    Rename example .elm file to match its module definition

--- examples/compileHelloWorld.js
+++ examples/compileHelloWorld.js
@@ -1,17 +1,17 @@
 var compile = require("../index.js").compile;
 var compileToString = require("../index.js").compileToString;
 
-compile(["./HelloWorld.elm"], {
+compile(["./Main.elm"], {
   output: "compiled-hello-world.js"
 }).on('close', function(exitCode) {
   console.log("Finished with exit code", exitCode);
 });
 
 
-compileToString(["./HelloWorld.elm"], {}).then(function(data){
+compileToString(["./Main.elm"], {}).then(function(data){
     console.log("compileToString produced a string with this length:", data.toString().length);
 });
 
-compileToString(["./HelloWorld.elm"], { output: "index.html" }).then(function(data){
+compileToString(["./Main.elm"], { output: "index.html" }).then(function(data){
     console.log("compileToString --output index.html produced a string with this length:", data.toString().length);
 });
